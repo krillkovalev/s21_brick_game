8a1b5ee76684384445cd077935060a2df7540810
Felicidad Daggerfall <felicidd@at-l7.msk.21-school.ru>
2024-03-11 20:48:32 +0300

    Figure is moving down, left, right

--- src/brick_game/tetris/start.c
+++ src/brick_game/tetris/start.c
@@ -1,3 +1,4 @@
+
 #include <ncurses.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,7 +10,7 @@
 #define OFFSET_Y 2
 
 #define FIELD_H 20
-#define FIELD_W 10 * 2
+#define FIELD_W 10
 
 
 typedef struct sketch_figure {
@@ -19,16 +20,14 @@ typedef struct sketch_figure {
 } sketch_figure;
 
 
-const sketch_figure figures[9] = {
-    {"00XXXXXXXX00", 6, 2}, // S-образная
-    {"XXXX0000XXXX", 6, 2}, // Z-образная
-    {"00XX00XXXXXX", 6, 2}, // T-образная
-    {"0000XXXXXXXX", 6, 2}, // L-образная
-    {"XX0000XXXXXX", 6, 2}, // зеркальная L
-    {"XXXXXXXX", 4, 2}, // квадрат
-    {"XXXXXXXX", 8, 1}, // прямая
-    {"XX", 2, 1}, // точка
-    {"XX00XXXX00XX", 4, 3} // точка
+const sketch_figure figures[7] = {
+    {"0XXXX0", 3, 2}, // S-образная
+    {"XX00XX", 3, 2}, // Z-образная
+    {"0X0XXX", 3, 2}, // T-образная
+    {"00XXXX", 3, 2}, // L-образная
+    {"X00XXX", 3, 2}, // зеркальная L
+    {"XXXX", 2, 2}, // квадрат
+    {"XXXX", 4, 1}, // прямая
 };
 
 typedef struct matrix_figure {
@@ -40,17 +39,20 @@ typedef struct matrix_figure {
 } matrix_figure;
 
 
-void draw_field(char field[20][20]);
+void draw_field(char (*field)[10]);
 void draw_figure(matrix_figure shape);
 void init_ncurses();
 void clear_window();
 void draw_border();
 matrix_figure create_figure(int number);
 char **create_matrix(int rows, int columns);
-int check_down_position(matrix_figure shape, char (*field)[20]);
-int check_left_position(matrix_figure shape, char (*field)[20]);
-int check_right_position(matrix_figure shape, char (*field)[20]);
-void part_of_ship(matrix_figure shape, char (*field)[20]);
+int get_random_shape(void);
+int check_down_position(matrix_figure shape, char (*field)[10]);
+int check_left_position(matrix_figure shape, char (*field)[10]);
+int check_right_position(matrix_figure shape, char (*field)[10]);
+void part_of_ship(matrix_figure shape, char (*field)[10]);
+matrix_figure rotate_figure(matrix_figure shape);
+void remove_figure(matrix_figure *shape);
 
 // Создаем фигуру
 matrix_figure create_figure(int number) {
@@ -92,6 +94,16 @@ char **create_matrix(int rows, int columns) {
   return matrix;
 }
 
+void remove_figure(matrix_figure *shape) {
+  memset(shape, 0, sizeof(*shape));
+  if (shape->work_figure != NULL) {
+    for (int i = 0; i < shape->rows; i++) {
+      free(shape->work_figure[i]);
+    }
+    free(shape->work_figure);
+  }
+
+}
 
 // Функция для инициализации ncurses
 void init_ncurses() {
@@ -123,7 +135,8 @@ void draw_figure(matrix_figure shape) {
     for (int i = 0; i < shape.rows; i++) {
         for (int j = 0; j < shape.columns; j++) {
 			if(shape.work_figure[i][j] == 'X') {
-            	mvaddch(shape.current_y + i, shape.current_x + j, ' '); 
+            	mvaddch(shape.current_y + i, shape.current_x + j * 2, ' '); 
+                mvaddch(shape.current_y + i, shape.current_x + j * 2 + 1, ' '); 
 				//mvaddch(y + i, x + j + 1, 'X'); // Рисуем маленькое окно без символов
 			}
         }
@@ -132,63 +145,67 @@ void draw_figure(matrix_figure shape) {
 }
 
 // Функция для отрисовки маленького окна зеленого цвета
-void draw_field(char field[20][20]) {
+void draw_field(char (*field)[10]) {
 
     init_pair(2, COLOR_WHITE, COLOR_WHITE); // Инициализация пары цветов
     init_pair(3, COLOR_WHITE, COLOR_BLUE); // Инициализация пары цветов
 
-    attron(COLOR_PAIR(3)); // Активация цвета
+    //attron(COLOR_PAIR(3)); // Активация цвета
     for (int i = 0; i < 20; i++) {
-        for (int j = 0; j < 20; j++) {
+        for (int j = 0; j < 10; j++) {
             if (field[i][j] != 'X') {
                 attron(COLOR_PAIR(2)); // Активация цвета
-                mvaddch(OFFSET_Y + i, OFFSET_X + j, ' '); // Рисуем маленькое окно без символов
+                mvaddch(OFFSET_Y + i, OFFSET_X + j * 2, ' '); // Рисуем маленькое окно без символов
+                mvaddch(OFFSET_Y + i, OFFSET_X + j * 2 + 1, ' ');
             }
             if (field[i][j] == 'X') {
                 attron(COLOR_PAIR(3)); // Активация цвета
-                mvaddch(OFFSET_Y + i, OFFSET_X + j, ' '); // Рисуем маленькое окно без символов
+                mvaddch(OFFSET_Y + i, OFFSET_X + j * 2, '['); // Рисуем маленькое окно без символов
+                mvaddch(OFFSET_Y + i, OFFSET_X + j * 2 + 1, ']');
             }
         }
     }
     attroff(COLOR_PAIR(2)); // Деактивация цвета
     attroff(COLOR_PAIR(3)); // Деактивация цвета
-    //refresh(); // Обновление отображения
+    // refresh(); // Обновление отображения
 }
 
 int main() {
+    // char field[20][20] = {0};
 
-     char field[20][20] = {
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '}, 
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X' ,'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X' ,'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' '},
-        {'X', 'X', 'X', 'X', ' ', ' ', ' ', ' ', 'X' ,'X', ' ', ' ', ' ', ' ', 'X', 'X', 'X', 'X', ' ' ,' '},
-        {'X', 'X', 'X', 'X', ' ', ' ', ' ', ' ', 'X' ,'X', ' ', ' ', ' ', ' ', 'X', 'X', 'X', 'X', ' ' ,' '},
+     char field[20][10] = {
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', }, 
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', },
+        {'X', 'X', 'X', ' ', 'X', ' ', ' ', ' ', 'X' ,'X', },
+        {'X', 'X', 'X', 'X', 'X', ' ', ' ', ' ', 'X' ,'X', },
     };
 
     init_ncurses(); // Инициализация ncurses
-	double timer = 1000000; //half second
+	double timer = 700000; //half second
 	struct timeval before, after;
     gettimeofday(&before, NULL);
 	nodelay(stdscr, TRUE); // Разрешить getch не блокировать
     //timeout(10000);
     // int x = 7, y = 1; // Стартовые координаты для фигуры
 
-    matrix_figure figure = create_figure(2);
+    int random_number_of_figure = get_random_shape();
+    matrix_figure figure = create_figure(random_number_of_figure);
 
     while (1) {
         //clear_window(); // Очистка окна
@@ -199,6 +216,7 @@ int main() {
 		if ((ch = getch()) != ERR) {
 			switch (ch) {
 				case KEY_UP: // Это будет поворот фигуры
+                    // matrix_figure rotate_figure(matrix_figure shape);
 					if (figure.current_y > OFFSET_Y) figure.current_y--;
 					break;
 				case KEY_DOWN:
@@ -207,11 +225,11 @@ int main() {
 					break;
 				case KEY_LEFT:
                     if (check_left_position(figure, field) == 0) figure.current_x -= 2;
-					//if (figure.current_x > OFFSET_X) figure.current_x -= 2;
+					// if (figure.current_x > OFFSET_X) figure.current_x -= 2;
 					break;
 				case KEY_RIGHT:
                     if (check_right_position(figure, field) == 0) figure.current_x += 2;
-					//if (figure.current_x < FIELD_W + OFFSET_X - figure.columns) figure.current_x += 2;
+					// if (figure.current_x < FIELD_W + OFFSET_X + figure.columns) figure.current_x += 2;
 					break;
 				case 'q': // Выход по нажатию 'q'
 					endwin(); // Завершение работы с ncurses
@@ -221,9 +239,13 @@ int main() {
         gettimeofday(&after, NULL);
         if (((double)after.tv_sec*100000000 + (double)after.tv_usec)-((double)before.tv_sec*100000000 + (double)before.tv_usec) > timer){ //time difference in microsec accuracy
             before = after;
-            // if (figure.current_y < FIELD_H + OFFSET_Y - figure.rows) figure.current_y++;
             if (check_down_position(figure, field) == 0) figure.current_y++;
-            else part_of_ship(figure, field);
+            else {
+                part_of_ship(figure, field);
+                remove_figure(&figure);
+                random_number_of_figure = get_random_shape();
+                figure = create_figure(random_number_of_figure);
+            }
         }
 
         
@@ -234,7 +256,7 @@ int main() {
 
 
 
-int check_down_position(matrix_figure shape, char (*field)[20]) {
+int check_down_position(matrix_figure shape, char (*field)[10]) {
     int answer_code = 0;
     if (shape.current_y == FIELD_H + OFFSET_Y - shape.rows) {
         answer_code = 1;
@@ -242,8 +264,7 @@ int check_down_position(matrix_figure shape, char (*field)[20]) {
         for(int i = shape.rows - 1; i >= 0; i--){
             for(int j = shape.columns - 1; j >= 0; j--){
                 if(shape.work_figure[i][j] == 'X') {
-                    if(field[i + shape.current_y - OFFSET_Y + 1][j + shape.current_x - OFFSET_X] == 'X'){
-                        // printw("                        Field = %d %d  Current = %d %d   I = %d J = %d\n", i + shape.current_x + OFFSET_X, j + shape.current_y + OFFSET_Y + 1, shape.current_x, shape.current_y, i, j);
+                    if(field[i + shape.current_y - OFFSET_Y + 1][j + shape.current_x / 2 - OFFSET_X / 2] == 'X'){
                         answer_code = 1;
                         break;
                     }
@@ -257,7 +278,7 @@ int check_down_position(matrix_figure shape, char (*field)[20]) {
 }
 
 
-int check_left_position(matrix_figure shape, char (*field)[20]) {
+int check_left_position(matrix_figure shape, char (*field)[10]) {
     int answer_code = 0;
     if (shape.current_x == OFFSET_X) {
         answer_code = 1;
@@ -265,7 +286,7 @@ int check_left_position(matrix_figure shape, char (*field)[20]) {
         for(int i = shape.rows - 1; i >= 0; i--){
             for(int j = shape.columns - 1; j >= 0; j--){
                 if(shape.work_figure[i][j] == 'X') {
-                    if(field[i + shape.current_y - OFFSET_Y][j + shape.current_x - OFFSET_X - 1] == 'X'){
+                    if(field[i + shape.current_y - OFFSET_Y][j + shape.current_x / 2 - OFFSET_X / 2 - 1] == 'X'){
                         // printw("                        Field = %d %d  Current = %d %d   I = %d J = %d\n", i + shape.current_x + OFFSET_X, j + shape.current_y + OFFSET_Y + 1, shape.current_x, shape.current_y, i, j);
                         answer_code = 1;
                         break;
@@ -280,16 +301,15 @@ int check_left_position(matrix_figure shape, char (*field)[20]) {
 }
 
 
-int check_right_position(matrix_figure shape, char (*field)[20]) {
+int check_right_position(matrix_figure shape, char (*field)[10]) {
     int answer_code = 0;
-    if (shape.current_x == FIELD_W + OFFSET_X - shape.columns) {
+    if (shape.current_x == (FIELD_W * 2 + OFFSET_X - shape.columns * 2)) {
         answer_code = 1;
     } else {
         for(int i = shape.rows - 1; i >= 0; i--){
             for(int j = shape.columns - 1; j >= 0; j--){
                 if(shape.work_figure[i][j] == 'X') {
-                    if(field[i + shape.current_y - OFFSET_Y][j + shape.current_x - OFFSET_X + 1] == 'X'){
-                        // printw("                        Field = %d %d  Current = %d %d   I = %d J = %d\n", i + shape.current_x + OFFSET_X, j + shape.current_y + OFFSET_Y + 1, shape.current_x, shape.current_y, i, j);
+                    if(field[i + shape.current_y - OFFSET_Y][j + shape.current_x / 2 - OFFSET_X / 2 + 1] == 'X'){
                         answer_code = 1;
                         break;
                     }
@@ -303,15 +323,46 @@ int check_right_position(matrix_figure shape, char (*field)[20]) {
 }
 
 
-void part_of_ship(matrix_figure shape, char (*field)[20]) {
+matrix_figure rotate_figure(matrix_figure shape) {
+  char **new_figure = create_matrix(shape.columns, shape.rows);
+  if (new_figure != NULL) {
+    for (int i = 0; i < shape.rows; i++) {
+      for (int j = 0; j < shape.columns; j++) {
+        new_figure[j][shape.rows - i - 1] = shape.work_figure[i][j];
+      }
+    }
+    remove_figure(&shape);
+    shape.work_figure = new_figure;
+    int temp = shape.rows;
+    shape.rows = shape.columns;
+    shape.columns = temp;
+  }
+  return shape;
+}
 
+
+void part_of_ship(matrix_figure shape, char (*field)[10]) {
     for (int i = 0; i < shape.rows; i++) {
         for (int j = 0; j < shape.columns; j++) {
-            if (shape.work_figure[i][j] == 'X'){
+            if (shape.work_figure[i][j] == 'X') {
                 field[i + shape.current_y - OFFSET_Y][j + shape.current_x - OFFSET_X] = 'X';
+                shape.work_figure[i][j] = ' ';
             }
         }
     }
+}
+
+int get_random_shape(void) {
+    time_t now;
+    struct tm *tm;
+
+    now = time(0);
+    if ((tm = localtime(&now)) == NULL) {
+        printf("Ошибка при извлечении времени\n");
+        return 1;
+    }
+
+    return tm->tm_sec % 7;
 }
 
 
