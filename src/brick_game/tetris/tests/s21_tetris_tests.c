65638a6c2c2dea0b351abd20b416206b3531db42
Felicidad Daggerfall <felicidd@il-k5.msk.21-school.ru>
2024-03-29 20:43:17 +0300

    Finite state machine is almost ready

---
+++ src/brick_game/tetris/tests/s21_tetris_tests.c
@@ -0,0 +1,9 @@
+// #include "s21_tetris_tests.h"
+
+// START_TEST(test_get_random_shape) {
+//     int result = get_random_shape();
+//     ck_assert_int_le(result, 7);
+//     ck_assert_int_ge(result, 1);
+// }
+// END_TEST
+
